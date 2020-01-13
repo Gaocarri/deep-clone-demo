75cbeefd33f72013b7546743edc89b47c0634e8f
GaoCarri <gaocarri@163.com>
2020-01-13 19:49:37 +0800

    注释

--- main.js
+++ main.js
@@ -4,16 +4,19 @@ function isObject(target) {
   return target !== null && type === 'object'
 }
 
-function deepClone(target, map = new WeakMap) {
+function deepClone(target, map = new WeakMap()) { // map是用来检查环（循环引用）
+  // 判断是不是对象
   if (isObject(target)) {
     // 判断是不是数组
     const cloneTarget = Array.isArray(target) ? [] : {}
     if (map.get(target)) {
-      return map.target
+      return map.get(target)
     }
+    // 用map存储克隆对象和原来对象的关系
     map.set(target, cloneTarget)
     for (let key in target) {
-      cloneTarget[key] = deepClone(target[key])
+      // 使用递归实现深拷贝（非递归可以浅拷贝）
+      cloneTarget[key] = deepClone(target[key], map)
     }
     return cloneTarget
   } else {
@@ -21,23 +24,28 @@ function deepClone(target, map = new WeakMap) {
   }
 }
 
+// 验证
 const obj1 = {
-  age: 18,
-  name: '高',
-  isMan: true,
-  house: null,
-  girlFriend: undefined,
-  number: [1, 3, 6, 8],
-  obj: {
-    book: 'JS语言精粹',
-    drink: '可口可乐'
-  },
-  sayHi() {
-    console.log('hi')
+    age: 18,
+    name: '高',
+    isMan: true,
+    house: null,
+    girlFriend: undefined,
+    number: [1, 3, 6, 8],
+    obj: {
+      book: 'JS语言精粹',
+      drink: '可口可乐'
+    },
+    sayHi() {
+      console.log('hi')
+    }
   }
-}
+  // 添加循环
+obj1.obj1 = obj1
 
-const obj2 = deepClone(obj1)
-
+// 开始拷贝
+const map = new Map()
+const obj2 = deepClone(obj1, map)
+  // 打印结果
 console.log(obj1);
 console.log(obj2);
